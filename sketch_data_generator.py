6a8adaf9c5ccfe284c77f2ca16d834817185e3aa
ljbelenky <ljbelenky@gmail.com>
2019-10-24 17:03:17 -0600

    working on ruled line generator

--- sketch_data_generator.py
+++ sketch_data_generator.py
@@ -6,6 +6,24 @@ from skimage import color
 import matplotlib.pyplot as plt
 
 
+class RulerGenerator:
+    def __init__(self, shape, line_width, spacing, raggedness, color, color_variation, angle):
+        # inner_width = int(line_width/3)
+        # outer_width = min(int(line_width), 1)
+        h, w = shape
+
+        basis = np.array([0]*spacing + [color]*line_width)
+
+        lines_array = np.array(
+            np.tile(basis, int((h*w)/len(basis)))).reshape(h, w)
+
+        return lines_array
+
+        # lines_array = np.zeros(shape=shape)
+
+        # number_of_lines = shape[0]/(spacing+line_width))
+
+
 class SketchDataGenerator():
     '''SketchDataGenerator yields pairs of sketches with/without ruled liens and image augmentation'''
 
